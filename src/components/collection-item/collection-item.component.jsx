9a9f4eb79cf574ed095c7e76260c9d9b0ae59e30
Sergey Laschotko <anaharet@tut.by>
2021-02-05 23:36:20 +0300

    added cart utils and add item action

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from "../custom-button/custom-button.component";
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
+
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +16,18 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton
+        inverted
+        onClick={() => addItem(item)}
+      >
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
